7d0d21c78719e50cf0cc6a8effbfdbac8ff1e96c
gycdgg <1165297373@qq.com>
2018-08-20 10:48:28 +0800

    support emoji

--- server/models/database.js
+++ server/models/database.js
@@ -8,7 +8,10 @@ const databaseInfo = {
   underscored: true,
   timezone: '+08:00',
   dialectOptions: {
-    ssl: false
+    charset: "utf8mb4",
+    collate: "utf8mb4_unicode_ci",
+    supportBigNumbers: true,
+    bigNumberStrings: true
   }
 }
 
